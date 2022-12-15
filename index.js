990515fb363429e043759e823d242fdb3290f9f4
João Pereira <joaompereira07@gmail.com>
2022-12-15 14:44:02 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const { Pool, Client } = require('pg')
+const dotenv = require("dotenv");
+dotenv.config()
+
+// psql Database credentials
+const credentials = {
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+};
+
+const client = new Pool(credentials);
+
+const port = 3000
+const host = '192.168.1.4'
+
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.render("check_appointments", { name: 'João' })
+  //response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/users', db.getUsers)
+
+app.get('/user/appointments/:id', (request, response) => {
+  const id = parseInt(request.params.id)
+  var appointments = getUserAppointments(id)
+    .then(appointments => { return appointments || []});
+  
+  //console.log(appointments)
+  appointments.then(function(result){
+    console.log(result);
+    response.render("check_appointments", { name: 'João', data: result })
+  })
+  
+  //response.render("check_appointments", appointments);
+})
+
+app.get('/user/:id', db.getUserById)
+
+app.post('/users', db.createUser)
+
+app.put('/users/:id', db.updateUser)
+
+app.delete('/users/:id', db.deleteUser)
+
+app.listen(port, host, () => {
+  console.log(`App running on port ${port}.`)
+})
+
+async function getUserAppointments(id){
+  //const id = parseInt(request.params.id)
+  try {
+    const res = await client.query('SELECT * FROM appointments WHERE id = $1', [id])
+    // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
+    return res.rows;
+  } catch (err) {
+    console.log(err.stack)
+  }
+
+}
